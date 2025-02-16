a489c98fa0c19f927eea1a34c5bc7c94943856cf
nagoal <nagoal@gmail.com>
2025-02-15 22:10:20 -0500

    Dejar el archivo final de datos eliminando los que no se usaron y actualizar README.md y app.py

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # exploracion_bosques
+En este proyecto se utilizan los datos de las Naciones Unidas actualizados a 2023 respecto al avance de todos los países y territorios en el logro de la meta de desarrollo sostenible número 15: Proteger, restablecer y promover el uso sostenible de los ecosistemas terrestres, gestionar sosteniblemente los bosques, luchar contra la desertificación, detener e invertir la degradación de las tierras y detener la pérdida de biodiversidad.
+Lo primero que hace esta aplicación es dar la posibilidad al usuario de pulsar un botón para generar un histograma con la información de extensión de bosques certificados por terceros a 2023.
+Luego permite pulsar un botón para generar un diagrama de dispersión con la información de la proporción de ecosistemas de montaña protegidos a corte de 2023 vs. el área de bosques certificada por terceros.
+Esta información es útil para profesionales y organizaciones trabajando en ella conservación, para identificar países con mayor potencial para sus actividades y para maximizar el impacto a nivel global. 
---
+++ app.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+
+st.header('Datos mundiales recopilados por Naciones Unidas sobre la meta de desarrollo sostenible número 15: Proteger, restablecer y promover el uso sostenible de los ecosistemas terrestres, gestionar sosteniblemente los bosques, luchar contra la desertificación, detener e invertir la degradación de las tierras y detener la pérdida de biodiversidad')  # poner un encabezado
+
+bosques_data = pd.read_csv('Data_SDG15.csv')  # leer los datos
+
+hist_button = st.button('Construir histograma de bosques')  # crear un botón
+
+if hist_button:  # al hacer clic en el botón
+    # escribir un mensaje
+    st.write(
+        'Creación de un histograma para el conjunto de datos de áreas certificadas de bosques')
+
+# crear un histograma
+    fig = px.histogram(bosques_data, x='Forest area certified under an independently verified certification scheme (thousands of hectares)',
+                       title='Extension de bosques certificados por terceros a 2023 (miles de hectareas)')
+
+# mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
+
+
+scatter_button = st.button(
+    'Construir diag. de dispersión de ecosistemas de montaña protegidos')  # crear un botón
+
+if scatter_button:  # al hacer clic en el botón
+ # escribir un mensaje
+    st.write('Creación de un diag. de dispersión para la porción de recursos ambientales clave de montaña que están protegidos vs. la extensión de los bosques certificados por agentes independientes')
+
+# crear un diagrama de dispersión
+    fig = px.scatter(bosques_data, x="Average proportion of Mountain Key Biodiversity Areas (KBAs) covered by protected areas (%)",
+                     y="Forest area certified under an independently verified certification scheme (thousands of hectares)")
+
+# mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
